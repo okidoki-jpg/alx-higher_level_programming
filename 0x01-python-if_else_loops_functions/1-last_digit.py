62343619ebc0247033f11a70102d4ecc3b845fc8
okidoki-jpg <okuhlen.20@gmail.com>
2022-11-30 11:37:50 +0200

    fixed pycodestyle & negative num output bug

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,8 +2,10 @@
 import random
 number = random.randint(-10000, 10000)
 n = abs(number) % 10
+if number < 0:
+    n *= -1
 
-print("Last digit of {:d} is {:d} and is".format(number, n), end = " ")
+print("Last digit of {:d} is {:d} and is".format(number, n), end=" ")
 if n > 5:
     print("greater than 5")
 elif n == 0:
