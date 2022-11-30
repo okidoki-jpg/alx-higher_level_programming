260e87bd6ce0d09236c28c990d1523a5e651ca38
okidoki-jpg <okuhlen.20@gmail.com>
2022-12-01 00:16:22 +0200

    task 13

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * check_cycle -  checks if a singly linked list has a cycle in it.
+ * @list: linked list to check.
+ * Return: 0 if there is no cycle, 1 if there is a cycle.
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *head, *node;
+
+	if (!list || !list->next)
+		return (0);
+
+	head = list->next;
+	node = list->next->next;
+	while (node->next)
+	{
+		if (head == node)
+			return (1);
+
+		head = head->next;
+		node = node->next->next;
+	}
+	return (0);
+}
