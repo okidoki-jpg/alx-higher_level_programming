11dc0a19726d6bf76b064efd665299643dbe89eb
okidoki-jpg <okuhlen.20@gmail.com>
2022-11-30 16:46:53 +0200

    fixed task 6 output

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-n, no = 2, 0
+n, no = 2, 1
 for i in range(100):
-    if (no != 0 and no % 10 == 0):
+    if (no % 10 == 0):
         no += n
         n += 1
     print("{:02d}".format(no), end='')
