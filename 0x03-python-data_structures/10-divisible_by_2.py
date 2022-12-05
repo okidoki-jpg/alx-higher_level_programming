e435d5774d38d0b022c5319d262dcef459096422
okidoki-jpg <okuhlen.20@gmail.com>
2022-12-05 16:30:45 +0200

    altered task 3, 11, 12

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
     def divisible_by_2(my_list=[]):
 
-        return [1 if i%2 == 0 else 0 for i in my_list]
+        return [True if i%2 == 0 else False for i in my_list]
--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
 
-    if idx > -1 and (idx < len(my_list) or idx == 0):
-        del my_list[idx]
+    if len(my_list) > 0:
+        if idx > -1 and (idx < len(my_list) or idx == 0):
+            del my_list[idx]
     return my_list
