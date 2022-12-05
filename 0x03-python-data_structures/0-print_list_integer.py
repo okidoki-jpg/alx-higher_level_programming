3f77c9d59b3c7faf5e3c68cda54bfc841f8c3529
okidoki-jpg <okuhlen.20@gmail.com>
2022-12-05 14:31:18 +0200

    altered task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     def print_list_integer(my_list=[]):
-        
-        if my_list:
-            for i in my_list:
-                print("{:d}".format(i))
+
+        for i in my_list:
+            print("{0:d}".format(i))
