0fc885b63f8f5aca57da5fcc6c9f877e77ae1957
okidoki-jpg <okuhlen.20@gmail.com>
2022-12-05 15:05:53 +0200

    altered task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def element_at(my_list, idx):
+def element_at(my_list, idx):
 
-        if idx > -1 and idx < len(my_list):
-            return my_list[idx]
-        return None
+    if idx > -1 and idx < len(my_list):
+        return my_list[idx]
+    return None
