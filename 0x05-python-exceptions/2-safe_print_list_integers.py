2f18b9fbdf02f27835aa91afc3f779444ad9dbb9
okidoki-jpg <okuhlen.20@gmail.com>
2022-12-20 03:58:08 +0200

    pep8 fix & better condition parameters

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,7 +2,7 @@
 
 def safe_print_list_integers(my_list=[], x=0):
 
-    i,idx = 0, 0
+    i, idx = 0, 0
 
     while i < x:
         try:
@@ -10,7 +10,7 @@ def safe_print_list_integers(my_list=[], x=0):
             i += 1
             idx += 1
         except (ValueError, TypeError):
-             i += 1
+            i += 1
         except IndexError:
             raise
     print()
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -7,7 +7,7 @@ def list_division(my_list_1, my_list_2, list_length):
         for i, j in zip(my_list_1, my_list_2):
             try:
                 res.append(i / j)
-            except TypeError:
+            except (TypeError, ValueError):
                 print("wrong type")
                 res.append(0)
             except ZeroDivisionError:
@@ -15,6 +15,6 @@ def list_division(my_list_1, my_list_2, list_length):
                 res.append(0)
         if dist > 0:
             print("out of range")
-            res.extend([0 for i in range(dist)])
+            res.extend([0 for i in range(list_length - len(res))])
     finally:
         return res
