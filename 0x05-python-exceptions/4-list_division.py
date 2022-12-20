0c2b7eadc654e728dea3ed0e3fc7b426d5a34255
okidoki-jpg <okuhlen.20@gmail.com>
2022-12-20 04:20:04 +0200

    spell correct

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -16,5 +16,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print("division by 0")
             div = 0
         finally:
-            rew.append(div)
+            res.append(div)
     return res
