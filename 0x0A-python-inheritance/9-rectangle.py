a220121a11aea2b176b55780626d5df5245330cb
Okuhle <okuhlen.20@gmail.com>
2023-01-10 00:24:04 +0000

    modded 9-re...py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,52 +1,20 @@
 #!/usr/bin/python
-"""
-Class Module Doc:  Rectangle
-
-Attributes:
-    BaseGeometry (class object): base class
-"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """
-    Class Doc: Defines Rectangle class
-
-    Methods:
-        __init__ - initialize obj
-        __str__ - return string rep of obj
-        area - calculates area of obj
-    """
 
     def __init__(self, width, height):
-        """
-        instantiate rectangle
-
-        Attributes:
-            width (int): rectangle width
-            height (int): rectangle height
-        """
 
         self.integer_validator("width", width)
-        self.__width = width
         self.integer_validator("height", height)
+        self.__width = width
         self.__height = height
 
     def __str__(self):
-        """
-        custom str method for print the rectangle
-
-        Return:
-            formated string
-        """
 
         return f"[Rectangle] {self.__width}/{self.__height}"
 
     def area(self):
-        """
-        calculates area of the retangle
 
-        Return: Rectangke area
-        """
-
-        return self.__width * self.__height
+        return (self.__width * self.__height)
