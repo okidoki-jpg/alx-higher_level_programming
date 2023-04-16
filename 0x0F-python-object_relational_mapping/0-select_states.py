2114ba466585320f390c49a38d992ad1ca87eb35
Okuhle <okuhlen.20@gmail.com>
2023-04-16 22:21:16 +0000

    modded 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,24 +5,24 @@ A script connecting to an SQL database
 """
 
 import sys
-from MySQLdb
+from MySQLdb import connect
 
 if __name__ == "__main__":
 
     args = sys.argv
 
     """ connect to database """
-    conn = MySQLdb.connect(host="localhost",
-                           user=args[1],
-                           password=args[2],
-                           database=args[3],
-                           port=3306)
+    conn = connect(host="localhost",
+                   user=args[1],
+                   password=args[2],
+                   database=args[3],
+                   port=3306)
 
     """ create cursor """
     c = conn.cursor()
 
     """ execute read and display query """
     c.execute("SELECT * FROM states")
-    print([i for i in c.fetchall()])
+    print([i for i in c.fetchall(), sep='\n')
     c.close()
     conn.close()
