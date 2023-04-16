9afb23e0c9ba0467ff4e304d06c9a1ceef5854f6
Okuhle <okuhlen.20@gmail.com>
2023-04-16 22:24:46 +0000

    modded 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -23,6 +23,6 @@ if __name__ == "__main__":
 
     """ execute read and display query """
     c.execute("SELECT * FROM `states` ORDER BY id ASC")
-    print(*c.fetchall(), sep='\n')
+    print([i for i in c.fetchall()])
     c.close()
     conn.close()
